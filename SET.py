53e7e96137174173239cb033b3b31c8728182fad
Jishnuraj2947 <jishnux29@gmail.com>
2025-09-18 17:05:24 +0530

    Add files via upload

---
+++ SET.py
@@ -0,0 +1,15 @@
+s1={1,2,3,4,"a","b","c"}
+s2={"a","b","c","d","e","f"}
+print("s1",s1)
+print("s2",s2)
+u=s1|s2
+i=s1&s2
+d=s1-s2
+sd=s1^s2
+print("union",u)
+print("intersection",i)
+print("difference",d)
+print("symmetric_difference",sd)
+
+
+
